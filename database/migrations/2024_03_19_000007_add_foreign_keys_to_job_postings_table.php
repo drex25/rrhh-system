e739f58269180ff42cb2e52968bf47f865f6a247
Sylvain Drexler Wilvins <tsgroup420@gmail.com>
2025-06-03 09:31:10 -0300

    modificar base job posting

---
+++ database/migrations/2024_03_19_000007_add_foreign_keys_to_job_postings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('job_postings', function (Blueprint $table) {
+            if (!Schema::hasColumn('job_postings', 'department_id')) {
+                $table->foreignId('department_id')->after('benefits')->constrained()->onDelete('cascade');
+            }
+            if (!Schema::hasColumn('job_postings', 'position_id')) {
+                $table->foreignId('position_id')->after('department_id')->constrained()->onDelete('cascade');
+            }
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('job_postings', function (Blueprint $table) {
+            if (Schema::hasColumn('job_postings', 'department_id')) {
+                $table->dropForeign(['department_id']);
+                $table->dropColumn('department_id');
+            }
+            if (Schema::hasColumn('job_postings', 'position_id')) {
+                $table->dropForeign(['position_id']);
+                $table->dropColumn('position_id');
+            }
+        });
+    }
+}; 
